680d6683d64d2042a863df2cc846659964fe4713
unknown <jahanshir.sm92@gmail.com>
2021-12-19 12:16:52 +0330

    add method signInManager and getMenuManager in class ManagerService

--- src/main/java/com/maktab/service/ManagerService.java
+++ src/main/java/com/maktab/service/ManagerService.java
@@ -1,4 +1,11 @@
 package com.maktab.service;
 
 public class ManagerService {
+    public boolean signInManager(String user,String password){
+        //TODO
+        return true;
+    }
+    public void getMenuManager(){
+        //TODO
+    }
 }
